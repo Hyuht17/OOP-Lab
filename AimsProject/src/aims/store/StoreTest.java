a42927c6b8be03817e9e7bac28e35d95f0b4c687
Hyuht17 <korofpc@gmail.com>
2024-11-25 19:04:27 +0700

    Update store

---
+++ AimsProject/src/aims/store/StoreTest.java
@@ -0,0 +1,21 @@
+package aims.store;
+
+import aims.disc.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args){
+        Store store = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Rager Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+
+        System.out.println("Number of DVDs in store: ");
+        store.listDVDs();
+
+        store.removeDVD("The Lion King");
+        System.out.println("Number of DVDs in store: ");
+        store.listDVDs();
+    }
+}
